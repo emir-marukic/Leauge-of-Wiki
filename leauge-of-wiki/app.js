844dba7a6a9f08604b052a4bb765305278f561e6
emir-marukic <marukicemir33@hotmail.com>
2023-09-02 18:13:46 +0200

    zavrsena main stranica

--- leauge-of-wiki/app.js
+++ leauge-of-wiki/app.js
@@ -14,23 +14,18 @@ fetch("http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json")
     for (const champion of Object.values(championsData)) {
       console.log(champion);
 
-      // Create a div element to hold the champion's image and name
       const championDiv = document.createElement("div");
       championDiv.classList.add("championBox");
-      // Create an img element for the champion's image
       const championImg = document.createElement("img");
       championImg.classList.add("championImg");
       championImg.src = `http://ddragon.leagueoflegends.com/cdn/13.17.1/img/champion/${champion.image.full}`;
 
-      // Create a paragraph element for the champion's name
       const championName = document.createElement("p");
       championName.textContent = champion.name;
 
-      // Append the image and name elements to the div
       championDiv.appendChild(championImg);
       championDiv.appendChild(championName);
 
-      // Append the div to the championList container
       championList.appendChild(championDiv);
 
       console.log(championDiv);
