16d02997e0d2f94dcf15635f2da9e61774feb26c
emir-marukic <marukicemir33@hotmail.com>
2023-09-03 17:31:53 +0200

    input filter field

--- leauge-of-wiki/app.js
+++ leauge-of-wiki/app.js
@@ -1,4 +1,6 @@
 const championList = document.querySelector(".championList");
+const searchInput = document.querySelector("input");
+let championsData = []; // Declare championsData outside the fetch block
 
 fetch("http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json")
   .then((response) => {
@@ -8,29 +10,40 @@ fetch("http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion.json")
     return response.json();
   })
   .then((data) => {
-    console.log(data.data);
-    const championsData = data.data;
-
-    for (const champion of Object.values(championsData)) {
-      console.log(champion);
-
-      const championDiv = document.createElement("div");
-      championDiv.classList.add("championBox");
-      const championImg = document.createElement("img");
-      championImg.classList.add("championImg");
-      championImg.src = `http://ddragon.leagueoflegends.com/cdn/13.17.1/img/champion/${champion.image.full}`;
-
-      const championName = document.createElement("p");
-      championName.textContent = champion.name;
-
-      championDiv.appendChild(championImg);
-      championDiv.appendChild(championName);
-
-      championList.appendChild(championDiv);
-
-      console.log(championDiv);
-    }
+    championsData = Object.values(data.data);
+    console.log(championsData);
+    updateChampionList(""); // Pass an empty string initially to display all champions
   })
   .catch((error) => {
     console.log("Fetch error", error);
   });
+
+function updateChampionList(inputValue) {
+  const filteredChampions = championsData.filter((champion) =>
+    champion.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  championList.textContent = "";
+
+  filteredChampions.forEach((champion) => {
+    const championDiv = document.createElement("div");
+    championDiv.classList.add("championBox");
+    const championImg = document.createElement("img");
+    championImg.classList.add("championImg");
+    championImg.src = `http://ddragon.leagueoflegends.com/cdn/13.17.1/img/champion/${champion.image.full}`;
+
+    const championName = document.createElement("p");
+    championName.textContent = champion.name;
+
+    championDiv.appendChild(championImg);
+    championDiv.appendChild(championName);
+
+    championList.appendChild(championDiv);
+  });
+}
+
+// Add an event listener to the input field
+searchInput.addEventListener("input", function () {
+  const inputValue = this.value.trim(); // Get the trimmed input value
+  updateChampionList(inputValue); // Update the displayed champion list
+});
